df272303f154433acb99fc08111d72f0d8ce4d92
Grant Johansen <grantscot654rw@byui.edu>
2026-09-22 13:20:35 -0600

    Letter Grade variable inserted and finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,31 +5,34 @@ class Program
     static void Main(string[] args)
     {
        String userGrade;
+       String letterGrade;
        Console.WriteLine ("What is your number grade?");
        userGrade = Console.ReadLine();
        int numberGrade = int.Parse(userGrade);
        if (numberGrade >= 90)
         {
-            Console.WriteLine ("Your letter grade is an A!");
+            letterGrade = "A";
         }
         else if (numberGrade < 90 && numberGrade >= 80)
         {
-            Console.WriteLine ("Your letter grade is an B!");
+            letterGrade = "B";     
         }
         else if (numberGrade < 80 && numberGrade >= 70)
         {
-            Console.WriteLine ("Your letter grade is an C.");
+            letterGrade = "C";
         }
         else if (numberGrade < 70 && numberGrade >= 60)
         {
-            Console.WriteLine ("Your letter grade is an D.");
+            letterGrade = "D";
         }
         else 
-            Console.WriteLine ("Your letter grade is F");
+            letterGrade = "F";
+
+        Console.WriteLine($"You got a {letterGrade}");
 
         if (numberGrade >= 70)
-            Console.WriteLine ("You passed this course!");
+            Console.WriteLine ("That means you passed this course!");
         else
-            Console.WriteLine ("You failed this City! Oh uh, I mean course... Try better next time.");
+            Console.WriteLine ("That means YOU FAILED THIS CITY! Oh uh, I mean course... Try better next time.");
     }   
 }
