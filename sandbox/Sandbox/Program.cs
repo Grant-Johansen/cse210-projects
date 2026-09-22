a4f84f0b1f6d429de1f307d0a3880d8a26bceac4
Grant Johansen <grantscot654rw@byui.edu>
2026-09-22 09:43:17 -0600

    If statment edits

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,11 +1,27 @@
 using System;
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("If you're gonna ride, ride in style!");
-        Console.WriteLine("Dang Daniel!");
+        int x =10;
+        int y =30;
+        int z =35;
+        if (x==10 && y == 30 || z == 35)
+        {
+            Console.WriteLine("X is 10");
+            Console.WriteLine("Y is fun");
 
+        }
+        else if (x==20)
+        {
+            Console.WriteLine("X is 20");
+        }
+        else
+        {
+            Console.WriteLine("Default output");
+        }
     }
 }
