d7d5400213921a070cb0c65678c5d2a98d9d4379
Grant Johansen <grantscot654rw@byui.edu>
2026-09-24 10:53:21 -0600

    Trying to sum list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string usernumber;
+        int listsum = 0;
+        int listaverage;
+        int smallestpositivenumber;
+
+        List<int> numbers = new List<int>();
+        List<int> sortednumnbers = new List<int>();
+        Console.WriteLine("Enter a list of nubmers, type 0 when finished.");
+
+        do
+        {
+            Console.WriteLine("Enter number:");
+            usernumber = Console.ReadLine();
+            int inputnumber = int.Parse(usernumber);
+            numbers.Add(inputnumber);
+        } while (usernumber != "0");
+
+        foreach (int number in numbers)
+        {
+            listsum += number;
+        }
+        Console.WriteLine($"The Sum of the numbers is {listsum}");
+
+
     }
 }
