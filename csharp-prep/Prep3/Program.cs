9cabfb9113476d203ba3fae021ff97b850b3ed86
Grant Johansen <grantscot654rw@byui.edu>
2026-09-24 10:24:45 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,9 +9,27 @@ class Program
         Console.WriteLine("What is the magic number?");
         usernumber = Console.ReadLine();
         int magicnumber = int.Parse(usernumber);
+        bool done = false;
+        do
+        {
+    
         Console.WriteLine("What is your guess?");
         userguess = Console.ReadLine();
         int guessnumber = int.Parse(userguess);
-        if ()
+        
+        if (guessnumber > magicnumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guessnumber < magicnumber)
+        {
+            Console.WriteLine("Higher");
+        }
+            else
+            {
+                Console.WriteLine("You got it!");
+                done = true;
+            }
+        } while (! done);
     }
 }
