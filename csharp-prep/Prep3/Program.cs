fab625e774afad2a992ee8456ee40ef4a295eb82
Grant Johansen <grantscot654rw@byui.edu>
2026-09-24 10:16:46 -0600

    Assignment updates

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        String usernumber;
+        String userguess;
+        Console.WriteLine("What is the magic number?");
+        usernumber = Console.ReadLine();
+        int magicnumber = int.Parse(usernumber);
+        Console.WriteLine("What is your guess?");
+        userguess = Console.ReadLine();
+        int guessnumber = int.Parse(userguess);
+        if ()
     }
 }
