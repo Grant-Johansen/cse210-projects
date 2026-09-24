de44e5a3bab3f0f1e2448e514bda3710db35e0b6
Grant Johansen <grantscot654rw@byui.edu>
2026-09-24 10:16:11 -0600

    List, and while loops.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,22 +6,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        int x =10;
-        int y =30;
-        int z =35;
-        if (x==10 && y == 30 || z == 35)
-        {
-            Console.WriteLine("X is 10");
-            Console.WriteLine("Y is fun");
+        // int x =10;
+        // int y =30;
+        // int z =35;
+        // if (x==10 && y == 30 || z == 35)
+        // {
+        //     Console.WriteLine("X is 10");
+        //     Console.WriteLine("Y is fun");
 
-        }
-        else if (x==20)
+        // }
+        // else if (x==20)
+        // {
+        //     Console.WriteLine("X is 20");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Default output");
+        // }
+
+        // bool done = false;
+        // while (! done)
+        // {
+        //     Console.Write("Are we done (y/n)? ");
+        //     done = Console.ReadLine() == "y";
+        // }
+        // bool done;
+        // do
+        // {
+        //     Console.Write("Are we done (y/n)");
+        //     done = Console.ReadLine() == "y";
+        // } while (! done);
+
+        for(int i =100; i > 0; i-=5)
         {
-            Console.WriteLine("X is 20");
+            Console.WriteLine(i);
         }
-        else
+
+        List<string> myFriends = new List<string>  {"Bob", "Betty", "Bubba"};
+
+        myFriends.Add("Doug");
+
+        foreach(string friend in myFriends)
         {
-            Console.WriteLine("Default output");
+            Console.WriteLine (friend);
         }
     }
 }
